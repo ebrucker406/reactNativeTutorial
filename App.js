e8bef4dbd4fa2acfdc0bbf4e622732280e4a6b75
Evan <ebrucker406@gmail.com>
2019-08-28 20:32:12 -0600

    State

--- App.js
+++ App.js
@@ -1,44 +1,32 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-class Greeting extends Component {
-  render() {
-    return (
-      <View style={{alignItems: "center"}}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    )
+class Blink extends Component {
+  componentDidMount(){
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000)
   }
-}
-
-export default class Bananas extends Component {
+  state = { isShowingText: true }
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    if (!this.state.isShowingText) {
+      return null
     }
+  
     return (
-      <View style={{alignItems: 'center', top: 50}}>
-        <Greeting name='Evan' />
-        <Greeting name='Bananas' />
-        <Image source={pic} style={{width: 193, height: 110}}/>
+      <Text>{this.props.text}</Text>  )
+  }
+}
+
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love blinking text' />
+        <Blink text='This will give you a seizure' />
       </View>
     )
   }
 }
-AppRegistry.registerComponent('FindMyRun', () => Bananas)
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
